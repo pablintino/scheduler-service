12f66e9e97e19c323826eee4ebd56319188f3820
Pablo Rodriguez Nava <info@pablintino.com>
2021-08-13 15:33:40 +0200

    + Created the main Maven structure.

--- scheduler-service/src/main/java/com/pablintino/schedulerservice/SchedulerServiceApplication.java
+++ scheduler-service/src/main/java/com/pablintino/schedulerservice/SchedulerServiceApplication.java
@@ -1,9 +1,12 @@
 package com.pablintino.schedulerservice;
 
+import com.pablintino.schedulerservice.config.CommonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(CommonConfiguration.class)
 public class SchedulerServiceApplication {
 
 	public static void main(String[] args) {
---
+++ scheduler-service/src/main/java/com/pablintino/schedulerservice/config/CommonConfiguration.java
@@ -0,0 +1,9 @@
+package com.pablintino.schedulerservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CommonConfiguration {
+
+
+}
