5b1e4fddaee2df44b8890dbf4550881f21f0e66d
Pablo Rodriguez Nava <info@pablintino.com>
2021-08-13 16:52:03 +0200

    + Start adding Quartz as a dependency and coding the basic configuration.

--- scheduler-service/src/main/java/com/pablintino/schedulerservice/SchedulerServiceApplication.java
+++ scheduler-service/src/main/java/com/pablintino/schedulerservice/SchedulerServiceApplication.java
@@ -1,12 +1,13 @@
 package com.pablintino.schedulerservice;
 
 import com.pablintino.schedulerservice.config.CommonConfiguration;
+import com.pablintino.schedulerservice.config.QuatzConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(CommonConfiguration.class)
+@Import(value = {CommonConfiguration.class, QuatzConfiguration.class})
 public class SchedulerServiceApplication {
 
 	public static void main(String[] args) {
