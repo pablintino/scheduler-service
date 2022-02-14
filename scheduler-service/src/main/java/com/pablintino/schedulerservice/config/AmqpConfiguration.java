74634e78fecfff18b6e969f08dbcf3a33e0d168d
Pablo Rodriguez Nava <pablintino@hotmail.com>
2022-02-15 00:58:04 +0100

    Added a startup call to RabbitMQ to start the template exchanges creation process

--- scheduler-service/src/main/java/com/pablintino/schedulerservice/config/AmqpConfiguration.java
+++ scheduler-service/src/main/java/com/pablintino/schedulerservice/config/AmqpConfiguration.java
@@ -1,15 +1,32 @@
 package com.pablintino.schedulerservice.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 
+import java.util.Map;
+
+@Slf4j
 @Configuration
 public class AmqpConfiguration {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     @Bean
     DirectExchange exchange(@Value("${com.pablintino.scheduler.amqp.exchange-name}") String exchangeName) {
         return new DirectExchange(exchangeName);
+    }
+
+    @EventListener({ContextRefreshedEvent.class})
+    void contextRefreshedEvent() {
+        Map<String, Object> serverProperties = rabbitTemplate.execute((channel) -> channel.getConnection().getServerProperties());
+        log.debug("Auto boot RabbitMQ call performed. Server details {}", serverProperties);
     }
 }
