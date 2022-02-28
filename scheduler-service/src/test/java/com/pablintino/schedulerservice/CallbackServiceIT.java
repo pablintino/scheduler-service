1fb2b434d54014131b6ec48e2b49e28008d1904f
Pablo Rodriguez Nava <pablintino@hotmail.com>
2022-02-28 21:57:00 +0100

    Added assertion to check task data object in the callback service

--- scheduler-service/src/test/java/com/pablintino/schedulerservice/CallbackServiceIT.java
+++ scheduler-service/src/test/java/com/pablintino/schedulerservice/CallbackServiceIT.java
@@ -1,7 +1,11 @@
 package com.pablintino.schedulerservice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pablintino.schedulerservice.callback.CallbackMessage;
 import com.pablintino.schedulerservice.configurations.AmqpTestIntegrationConfiguration;
+import com.pablintino.schedulerservice.helpers.DummyTaskData;
 import com.pablintino.schedulerservice.helpers.HttpCallbackMockController;
 import com.pablintino.schedulerservice.models.CallbackType;
 import com.pablintino.schedulerservice.models.ScheduleJobMetadata;
@@ -10,7 +14,6 @@ import com.pablintino.schedulerservice.services.ICallbackService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.quartz.JobDataMap;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +25,10 @@ import org.springframework.test.annotation.DirtiesContext;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -31,9 +38,13 @@ import java.util.concurrent.TimeUnit;
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = {"com.pablintino.scheduler.http.callback-timeout:5000"})
 class CallbackServiceIT {
+
+  private final TypeReference<HashMap<String, Object>> OBJECT_MAPPER_MAP_TYPE =
+      new TypeReference<>() {};
+
   @Autowired private ICallbackService callbackService;
-  @Autowired private DirectExchange exchange;
   @Autowired private HttpCallbackMockController httpCallbackMockController;
+  @Autowired private ObjectMapper objectMapper;
 
   @LocalServerPort private int serverPort;
 
@@ -56,8 +67,16 @@ class CallbackServiceIT {
   private void createAssertSimpleSendOK(
       BlockingQueue<CallbackMessage> messageQueue, CallbackType type)
       throws InterruptedException, UnknownHostException {
-    JobDataMap map = new JobDataMap();
-    map.put("test-key", "test");
+
+    DummyTaskData taskData = new DummyTaskData();
+    taskData.setTestDate(Date.from(Instant.now()));
+    taskData.setTestFloat(1.123F);
+    taskData.setTestInt(33);
+    taskData.setTestList(Arrays.asList("test-1", "test-2", "test-3"));
+    Map<String, Integer> testMap = new HashMap<>();
+    testMap.put("test-1", 2);
+    taskData.setTestMap(testMap);
+
     ScheduleJobMetadata scheduleEventMetadata = new ScheduleJobMetadata();
     scheduleEventMetadata.setNotificationAttempt(1);
     scheduleEventMetadata.setTriggerTime(Instant.now());
@@ -75,17 +94,25 @@ class CallbackServiceIT {
             type,
             scheduleEventMetadata);
 
-    callbackService.executeCallback(schedulerJobData, map);
+    callbackService.executeCallback(schedulerJobData, taskData);
 
     CallbackMessage message = messageQueue.poll(5, TimeUnit.SECONDS);
     Assertions.assertNotNull(message);
     Assertions.assertEquals(schedulerJobData.getKey(), message.getKey());
     Assertions.assertEquals(schedulerJobData.getTaskId(), message.getId());
-    // todo adapt
-    // Assertions.assertEquals(map.getWrappedMap(), message.getDataMap());
     Assertions.assertEquals(
         scheduleEventMetadata.getNotificationAttempt(), message.getNotificationAttempt());
     Assertions.assertEquals(scheduleEventMetadata.getTriggerTime(), message.getTriggerTime());
+
+    /* At this point object type is unknown, so it serializes to Map<String, Object> */
+    Assertions.assertNotNull(message.getData());
+    try {
+      Assertions.assertEquals(
+          objectMapper.readValue(objectMapper.writeValueAsString(taskData), OBJECT_MAPPER_MAP_TYPE),
+          message.getData());
+    } catch (JsonProcessingException ex) {
+      Assertions.fail("Cannot serialize/deserialize dummy data");
+    }
   }
 
   @RabbitListener(queues = {AmqpTestIntegrationConfiguration.QUEUE_KEY})
